e4a44769c74ef88dec83b848351d6ae4bc62c374
akajsaunders <me@jsaunders.dev>
2022-12-01 22:56:29 +0000

    Complete day 1 challenge

---
+++ challenges/day-1-calorie-counting/answer.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "sort"
+)
+
+func main() {
+    readFile, err := os.Open("challenges/day-1-calorie-counting/puzzle-input.txt")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fileScanner := bufio.NewScanner(readFile)
+
+    fileScanner.Split(bufio.ScanLines)
+
+    elf_calories := make([]int, 0)
+
+    var elf_calorie_count = 0
+
+    for fileScanner.Scan() {
+
+        if fileScanner.Text() == "" {
+            elf_calories = append(elf_calories, elf_calorie_count)
+            elf_calorie_count = 0
+        } else {
+            calorie, _ := strconv.Atoi(fileScanner.Text())
+            if err != nil {
+                fmt.Println("Error during conversion")
+                return
+            }
+
+            elf_calorie_count += calorie
+        }
+
+    }
+
+    readFile.Close()
+
+    
+    sort.Sort(sort.Reverse(sort.IntSlice(elf_calories)))
+
+    fmt.Println("Most calories being carried: ", elf_calories[0])
+    fmt.Println("Total calories carried by the top 3:", elf_calories[0] + elf_calories[1] + elf_calories[2])
+}
